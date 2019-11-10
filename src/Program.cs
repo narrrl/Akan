00f51547dca2da60672d0af21d3c17845e7b47a1
Nils Pukropp <nils.pukropp@outlook.de>
2019-11-10 01:50:59 +0100

    Create Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Webhook;
+
+namespace AkanDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        }
+    }
+}
