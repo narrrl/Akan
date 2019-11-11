111975b1237ce9d2aca596d09553fe501c1010a6
Nils Pukropp <nils.pukropp@outlook.de>
2019-11-11 03:29:45 +0100

    ChatModules/Program/ChatService/CommandHandler/LoggingService
    Updated some files

---
+++ src/Services/ChatService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.IO;
+
+namespace AkanDiscordBot.Services
+{
+    class ChatService
+    {
+    }
+}
