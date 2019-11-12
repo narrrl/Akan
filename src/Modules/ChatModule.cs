64fbf66f91bbd44f534826f6c5858a317efc4303
Nirusu99 <Nils.Pukropp@outlook.de>
2019-11-12 17:16:55 +0100

    Update ChatModule.cs

--- src/Modules/ChatModule.cs
+++ src/Modules/ChatModule.cs
@@ -18,7 +18,8 @@ namespace AkanDiscordBot.Modules
             // ~say hello world -> hello world
             [Command("say")]
             [Summary("Echoes a message.")]
-            public Task SayAsync([Remainder] [Summary("The text to echo")] string echo);
+            public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+                => ReplyAsync(echo);
 
             // ReplyAsync is a method on ModuleBase 
         }
